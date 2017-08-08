21a3e9b05bbf6c818717ec42b04a169e50f8c1ca
ahmed <kira@gmail.com>
2017-08-08 17:39:05 +0200

    assert.fail() added in async task test catch clause

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
@@ -3,6 +3,8 @@ package com.udacity.gradle.builditbigger;
 import android.support.test.runner.AndroidJUnit4;
 
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -21,13 +23,17 @@ public class AsyncTaskTest {
     @Test
     public void verifyAsyncTaskResult(){
         EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask();
+
         String joke = null;
         try {
             joke = endpointsAsyncTask.execute().get();
         } catch (InterruptedException e) {
             e.printStackTrace();
+            Assert.fail();
+
         } catch (ExecutionException e) {
             e.printStackTrace();
+            Assert.fail();
         }
 
         assertNotNull(joke);
